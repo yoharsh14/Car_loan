d6637051c8d12564627e4c6d2f1d9bf1225d7001
yoharsh14 <86423823+yoharsh14@users.noreply.github.com>
2024-12-11 00:33:11 +0530

    updated

---
+++ Loan Workflow/src/main/java/com/example/workflow/workflowServices/Enjoy.java
@@ -0,0 +1,12 @@
+package com.example.workflow.workflowServices;
+
+import jakarta.inject.Named;
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+@Named
+public class Enjoy implements JavaDelegate {
+    @Override
+    public void execute(DelegateExecution execution) throws Exception {
+
+    }
+}
